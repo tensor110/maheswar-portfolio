975640d6c767fe06def69b1ce9e5573c06af1b5b
Maheswar Mahapatra <maheswar2110@gmail.com>
2024-09-23 23:59:25 +0530

    Resume page done

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -51,24 +51,23 @@ const Services = () => {
             return( 
             <div 
               key = {index}
-              className="flex-1 flex flex-col justify-center gap-6 group"
+              className="flex-1 flex flex-col justify-center gap-6 group w-4/5"
             >
               <div className="w-full flex justify-between items-center">
-                <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{service.num}</div>
+                <div className="text-4xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{service.num}</div>
                 <Link href = {service.href}
-                  className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:rotate-45" 
+                  className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45" 
                 >
                   <BsArrowDownRight className="text-primary text-3xl" />
                 </Link>
               </div>
-              <h2>{service.title}</h2>
-              <p>{service.description}</p>
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">{service.title}</h2>
+              <p className="text-white/60">{service.description}</p>
               <div className="border-b border-white/20 w-full"></div>
             </div>
             )
           })}
         </motion.div>
-        <h1>Hello</h1>
       </div> 
     </section>
   )
